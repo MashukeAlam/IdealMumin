eed55ab8263895e68d12d11b43fea2e091ea044e
Mashuke Alam Jim <jimmashuke@gmail.com>
2024-02-07 12:10:33 +0600

    one icon change and failed to implement kaaba and medinah icons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,6 @@ import { useEffect, useState } from 'react';
 import { router } from 'expo-router';
 import CircularNumberContainer from './surahs/SurahNumberContainer';
 
-
 const DATA = [
   {
     title: "First Item",
