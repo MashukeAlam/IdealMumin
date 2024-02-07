521ae5d73592d3a69e0d9ad978546c4a12576811
Mashuke Alam Jim <jimmashuke@gmail.com>
2024-02-07 11:50:13 +0600

    format document

--- app/(tabs)/surahs/[id].tsx
+++ app/(tabs)/surahs/[id].tsx
@@ -1,75 +1,72 @@
-import { Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
+import { Dimensions, StyleSheet } from 'react-native';
 import { Text, View } from '@/components/Themed';
 import { FlashList } from '@shopify/flash-list';
 import { useEffect, useState } from 'react';
 import { useLocalSearchParams } from 'expo-router';
 import CircularNumberContainer from './SurahNumberContainer';
-import { BlurView } from 'expo-blur';
 import Storage from 'expo-storage';
 
 function Surah() {
     const { id } = useLocalSearchParams<{ id: string }>();
     const [surahData, setSurahData] = useState(null);
-    
+
     useEffect(() => {
         const fetchData = async () => {
-        try {
-            const response = await fetch(`https://api.alquran.cloud/v1/surah/${id}`);
-            if (!response.ok) {
-            throw new Error('Network response was not ok');
-            }
+            try {
+                const response = await fetch(`https://api.alquran.cloud/v1/surah/${id}`);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
 
-            const data = await response.json();
-            setSurahData(data.data.ayahs);
-            await Storage.setItem({
-                key: `${id}`,
-                value: JSON.stringify(data.data.ayahs)
-            });
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
+                const data = await response.json();
+                setSurahData(data.data.ayahs);
+                await Storage.setItem({
+                    key: `${id}`,
+                    value: JSON.stringify(data.data.ayahs)
+                });
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         };
 
         const configureData = async (key: string) => {
-            const jsonValue = await Storage.getItem({key: key});
-            
+            const jsonValue = await Storage.getItem({ key: key });
+
             if (jsonValue !== null) {
-              console.log("No loading was needed.");
-              setSurahData(JSON.parse(jsonValue));
-              
+                console.log("No loading was needed.");
+                setSurahData(JSON.parse(jsonValue));
+
             } else {
-              console.log("Fresh loading was needed this time.");
-              fetchData();
+                console.log("Fresh loading was needed this time.");
+                fetchData();
             }
-          }
-          
-          configureData(`${id}`);
+        }
 
-    fetchData();
+        configureData(`${id}`);
 
-    return () => {
-        setSurahData(null);
-    }
-  }, [id]);
+        fetchData();
+
+        return () => {
+            setSurahData(null);
+        }
+    }, [id]);
     return (
         <View style={{ height: Dimensions.get("window").height, width: Dimensions.get("screen").width }}>
-      {surahData != null ? 
-      <FlashList
-        data={surahData}
-        renderItem={({ item }) => <View style={styles.listContainer}><CircularNumberContainer style={styles.ayahNumber} ayahNumber={item.numberInSurah}/><Text style={styles.textStyle}>{item.text}</Text></View>}
-        estimatedItemSize={200}
-      /> : <Text>Not loaded</Text>}
- </View>
+            {surahData != null ?
+                <FlashList
+                    data={surahData}
+                    renderItem={({ item }) => (<View style={styles.listContainer}><CircularNumberContainer style={styles.ayahNumber} ayahNumber={item.numberInSurah} /><Text style={styles.textStyle}>{item.text}</Text></View>)}
+                    estimatedItemSize={200}
+                /> : <Text>Not loaded</Text>}
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
-    textStyle : {
+    textStyle: {
         fontSize: 20
     },
-    listContainer : {
+    listContainer: {
         padding: 10,
         marginRight: 5,
         flex: 1,
@@ -82,7 +79,7 @@ const styles = StyleSheet.create({
         bottom: 0,
         left: 0,
         right: 0,
-      },
+    },
 })
 
 export default Surah;
